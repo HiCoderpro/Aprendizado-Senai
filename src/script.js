7d00a51eedf7c4d7ce3348b2d81e2d9ec3f76012
048lucasoliveira <lucasemfloripa@gmail.com>
2023-10-26 18:19:04 -0300

    criando Repositorio Git

---
+++ src/script.js
@@ -0,0 +1,17 @@
+function exemplo(){
+
+    var nome = '123'
+    console.log(nome)
+
+    if (true){
+
+        const data = new Date();
+
+        console.log(data);
+    }
+
+}
+
+const nome = 'Moiseis rocha '
+console.log(nome)
+exemplo();
