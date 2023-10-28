547cdeeb38f4729ce984afee3bae9727347b118c
048Lucasoliveira <148930690+048Lucasoliveira@users.noreply.github.com>
2023-10-28 19:33:00 -0300

    Update index

--- src/script.js
+++ src/script.js
@@ -1,17 +1,2 @@
-function exemplo(){
-
-    var nome = '123'
-    console.log(nome)
-
-    if (true){
-
-        const data = new Date();
-
-        console.log(data);
-    }
-
-}
-
-const nome = 'Moiseis rocha '
-console.log(nome)
-exemplo();
+let y = 5;
+y = 10; // Isso é permitido
